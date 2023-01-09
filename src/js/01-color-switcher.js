14c3455782182fcaf5a2e92980d6352da3dc0fdc
Hanna Tubolets <anna.tubolets@gmail.com>
2023-01-09 20:14:59 +0300

    Added js-01-task-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+btnStopEl.disabled = true;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStartEl.addEventListener('click', () => {
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+btnStopEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+});
 
